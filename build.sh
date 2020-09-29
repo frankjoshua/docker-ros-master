8b0c9f2c68abf56dab32dadb51c7ea647bf741dc
josh <josh@tesseractmobile.com>
2020-09-28 23:16:55 -0500

    Checking for valid input in build script

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+docker buildx version
+if [[ $? -ne 0 ]]; then
+echo "Docker must have experimental features enabled"
+exit 1
+fi
+if [[ $# -lt 1 ]]; then
+echo "Usage: build.sh CONTAINER_NAME [--push]"
+exit 1
+fi
 BUILDER=$(docker buildx create --use)
 docker buildx build -t $1 --platform linux/arm/v7,linux/arm64/v8,linux/amd64 . $2
 docker buildx rm $BUILDER
