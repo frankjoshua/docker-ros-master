f91185c56b1c148b5b07f0d0f604a58b90773696
josh <josh@tesseractmobile.com>
2020-09-28 23:52:10 -0500

    Added linux/arm

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ echo "Usage: build.sh CONTAINER_NAME [--push]"
 exit 1
 fi
 BUILDER=$(docker buildx create --use)
-docker buildx build -t $1 --platform linux/arm/v7,linux/arm64/v8,linux/amd64 . $2
+docker buildx build -t $1 --platform linux/arm,linux/arm/v7,linux/arm64/v8,linux/amd64 . $2
 docker buildx rm $BUILDER
